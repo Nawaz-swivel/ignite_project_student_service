549ad2ca2162f08da24d5e45d02dfe97e487a46a
Nawaz-swivel <nawas@swivelgroup.com.au>
2023-04-02 00:52:08 +0530

    rm main test

--- src/test/java/com/swivel/ignite/student/StudentApplicationTests.java
+++ src/test/java/com/swivel/ignite/student/StudentApplicationTests.java
@@ -14,10 +14,10 @@ class StudentApplicationTests {
     /**
      * This method tests Spring application main run method
      */
-    @Test
-    void Should_RunSpringApplication() {
-        StudentApplication.main(new String[]{});
-
-        assertTrue(true, "Spring Application Context Loaded Successfully");
-    }
+//    @Test
+//    void Should_RunSpringApplication() {
+//        StudentApplication.main(new String[]{});
+//
+//        assertTrue(true, "Spring Application Context Loaded Successfully");
+//    }
 }
